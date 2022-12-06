4ed8238d9eb41851f3d06f1e5bde58a58fad4aa0
Caleb Thomas <cjthomas00@comcast.net>
2022-12-06 15:47:23 -0700

    feat: complete methods for find_all.

--- practice/find_all.rb
+++ practice/find_all.rb
@@ -13,6 +13,7 @@ def find_all_even_numbers # done for you
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     numbers.find_all do |number|
+      number.odd?
       # Your code goes here
     end
 
@@ -22,7 +23,9 @@ def find_all_even_numbers # done for you
   def find_all_words_with_three_letters
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
 
-    # Your code goes here
+    words.find_all do |word|
+      word.length == 3
+    end
 
     #  expected return value is ["bad", "cat", "dog", "red"]
   end
@@ -30,15 +33,18 @@ def find_all_even_numbers # done for you
   def find_all_words_with_more_than_three_letters
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
 
-    # Your code goes here
-
+    words.find_all do |word|
+      word.length > 3
+    end
     #  expected return value is ["pill", "finger", "blue", "table"]
   end
 
   def find_all_words_ending_in_e
     words = ["are", "you", "strike", "thinking", "belt", "piece", "warble", "sing", "pipe"]
 
-    # Your code goes here
+    words.find_all do |word|
+      word.end_with?('e')
+    end
 
     #  expected return value is ["are", "strike", "piece", "warble", "pipe"]
   end
@@ -46,15 +52,18 @@ def find_all_even_numbers # done for you
   def find_all_words_ending_in_ing
     words = ["bring", "finger", "drought", "singing", "bingo", "purposeful"]
 
-    # Your code goes here
-
+    words.find_all do |word|
+      word.end_with?('ing')
+    end
     #  expected return value is ["bring", "singing"]
   end
 
   def find_all_words_containing_e
     words = ["four", "red", "five", "blue", "pizza", "purple"]
 
-    # Your code goes here
+    words.find_all do |word|
+      word.include?('e')
+    end
 
     #  expected return value is ["red", "five", "blue", "purple"]
   end
@@ -62,7 +71,9 @@ def find_all_even_numbers # done for you
   def find_all_dinosaurs
     animals = ["tyrannosaurus", "narwhal", "eel", "achillesaurus", "qingxiusaurus"]
 
-    # Your code goes here
+    animals.find_all do |word|
+      word.include?('saurus')
+    end
 
     #  expected return value is ["tyrannosaurus", "achillesaurus", "qingxiusaurus"]
   end
@@ -70,15 +81,18 @@ def find_all_even_numbers # done for you
   def find_all_floats
     numbers = [3, 1.4, 3.5, 2, 4.9, 9.1, 8.0]
 
-    # Your code goes here
-
+    numbers.find_all do |numb|
+        numb.is_a?(Float)
+    end
     #  expected return value is [1.4, 3.5, 4.9, 9.1, 8.0]
   end
 
   def find_all_arrays
     elements = ["CAT", ["dog"], 23, [56, 3, 8], "AIMLESS", 43, "butter"]
 
-    # Your code goes here
+    elements.find_all do |element|
+      element.is_a?(Array)
+    end
 
     #  expected return value is [["dog"], [56, 3, 8]]
   end
@@ -86,21 +100,22 @@ def find_all_even_numbers # done for you
   def find_all_hashes
     elements = ["cat", {:dog=>"fido"}, 23, {:stuff=>"things"}, "aimless", 43]
 
-    # Your code goes here
-
+    elements.find_all do |element|
+      element.is_a?(Hash)
+    end
     #  expected return value is [{:dog=>"fido"}, {:stuff=>"things"}]
   end
 
 
 
-# p find_all_even_numbers
-# p find_all_odd_numbers
-# p find_all_words_with_three_letters
-# p find_all_words_with_more_than_three_letters
-# p find_all_words_ending_in_e
-# p find_all_words_ending_in_ing
-# p find_all_words_containing_e
-# p find_all_dinosaurs
-# p find_all_floats
-# p find_all_arrays
-# p find_all_hashes
+ p find_all_even_numbers
+ p find_all_odd_numbers
+ p find_all_words_with_three_letters
+ p find_all_words_with_more_than_three_letters
+ p find_all_words_ending_in_e
+ p find_all_words_ending_in_ing
+ p find_all_words_containing_e
+ p find_all_dinosaurs
+ p find_all_floats
+ p find_all_arrays
+ p find_all_hashes
